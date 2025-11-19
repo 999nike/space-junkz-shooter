3e84ebc85e7d9b409ad4e1b5f4e1017b88ff192e
999nike <999nike@gmail.com>
2025-11-19 01:51:51 +0000

    Update game.js

--- src/game/game.js
+++ src/game/game.js
@@ -4,12 +4,12 @@
   const S = window.GameState;
 
   window.initGame = function initGame() {
-   S.canvas = document.getElementById("game");
-   S.ctx = S.canvas.getContext("2d");
-   S.W = S.canvas.width;
-   S.H = S.canvas.height;
+    S.canvas = document.getElementById("game");
+    S.ctx = S.canvas.getContext("2d");
+    S.W = S.canvas.width;
+    S.H = S.canvas.height;
 
-   // Load ship sprite
+    // Load ship sprite
     S.shipImage = new Image();
     S.shipImage.src = "./src/game/AlphaFighter.png";   // your file
 
