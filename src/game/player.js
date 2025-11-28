bbb4687faec08113b1ad5bbff9b3e0e0e48602b2
999nike <999nike@gmail.com>
2025-11-28 18:15:23 +0000

    S

--- src/game/player.js
+++ src/game/player.js
@@ -57,7 +57,7 @@ window.drawPlayer = function drawPlayer(ctx) {
 
     // Position behind/below ship
     const flameOffsetX = -6;    // move left
-    const flameOffsetY = 155;   // move down/back
+    const flameOffsetY = 110;   // move down/back
 
     // Flame size
     const boosting = (S.keys?.["w"] || S.keys?.["arrowup"]);
